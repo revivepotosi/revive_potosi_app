219be5385c201f70582d8e078263bc20b48318ee
Hugo Patricio Quispe Pacaje <hquispe.3it@bancochile.cl>
2023-12-05 01:07:45 -0400

    feat: se agrega el boton para probar AR

--- src/feature/explore/containers/ExploreScreen.js
+++ src/feature/explore/containers/ExploreScreen.js
@@ -1,8 +1,21 @@
 import { Text } from '@ui-kitten/components';
 import React from 'react';
+import { View } from 'react-native';
+import Style from '../../../style/Style';
+import Button from '../../../components/Button';
 
-const ExploreScreen = () => (
-    <Text>Explorar</Text>
-);
+const ExploreScreen = () => {
+    const goToAR = () => {
+        console.log('Go To AR')
+    };
+
+    return (
+        <View style={Style.p_4}>
+            <Button onPress={goToAR}>
+                Ir a AR
+            </Button>
+        </View>
+    );
+};
 
 export default ExploreScreen;
